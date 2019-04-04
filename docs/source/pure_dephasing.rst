859596d2112e8be540618164d067db79fc5f4e47
quantshah <shahnawaz.ahmed95@gmail.com>
2019-04-04 17:37:49 +0200

    Added pure dephasing

--- docs/source/pure_dephasing.rst
+++ docs/source/pure_dephasing.rst
@@ -1,3 +1,57 @@
-######################
-Pure dephasing vs Heom
-######################
+##############
+Pure dephasing
+##############
+
+The analytical evolution of the density matrix in the pure dephasing case is given by
+
+.. image:: images/pd.png
+
+where F(t) is an integral which can be computed analytically in the zero temperature limit given that the correlation can be expressed as a sum of exponents. This analytical formula uses the Matsubara and non-Matsubara fitting exponents.
+
+The exact evolution can be computed directly by a double integration of the correlation function from the underdamped Brownian motion spectral density. We can now compare both the results to see the error in the dynamics due to the fitting procedure.
+
+We switch to a frame rotating with the qubit frequency and can thus set it to zero to see the dynamics due to the interaction only.
+
+.. code-block:: python
+
+	import numpy as np
+	from numpy.testing import (run_module_suite, assert_,
+	                           assert_array_almost_equal, assert_raises)
+
+	from matsubara.pure_dephasing import (pure_dephasing_integrand,
+	                                      pure_dephasing_evolution,
+	                                      pure_dephasing_evolution_analytical)
+	from matsubara.correlation import (biexp_fit, nonmatsubara_exponents, matsubara_exponents,
+	                                   matsubara_zero_analytical)
+
+	import matplotlib.pyplot as plt
+
+
+	coup_strength, cav_broad, cav_freq = .8, .4, 1.
+
+	tlist = np.linspace(0, 100, 200)
+
+	# Set qubit frequency to 0 to see only the dynamics due to the interaction.
+	wq = 0
+	# Zero temperature case
+	beta = np.inf
+	ck1, vk1 = nonmatsubara_exponents(coup_strength, cav_broad, cav_freq, beta)
+
+	mats_data_zero = matsubara_zero_analytical(coup_strength, cav_broad, cav_freq, tlist)
+	ck20, vk20 = biexp_fit(tlist, mats_data_zero)
+
+	ck = np.concatenate([ck1, ck20])
+	vk = np.concatenate([vk1, vk20])
+
+	pd_analytical = pure_dephasing_evolution(tlist, coup_strength, cav_broad,
+	                                         cav_freq, beta, wq)
+	pd_numerical_fitting = pure_dephasing_evolution_analytical(tlist, wq, ck, vk)
+
+	plt.plot(tlist, pd_analytical, color="b", label="Analytical integral")
+	plt.plot(tlist, pd_numerical_fitting, color="r", label = "Integral with exponenents")
+	plt.xlabel("t")
+	plt.ylabel(r"$\rho_{01}$($w_q = 0$)")
+	plt.legend()
+	plt.show()
+
+.. image:: examples/plots/pd.png
---
+++ matsubara/pure_dephasing.py
@@ -0,0 +1,108 @@
+"""
+Analytical computation of the dynamics for the pure dephasing case. 
+"""
+import numpy as np
+
+from matsubara.correlation import underdamped_brownian, coth
+
+from scipy.integrate import quad
+
+
+def pure_dephasing_integrand(w, coup_strength, cav_broad, cav_freq, beta, t):
+    """
+    Calculates the pure dephasing integrand.
+    
+    Parameters
+    ----------
+    coup_strength: float
+        The coupling strength parameter.
+
+    cav_broad: float
+        A parameter characterizing the FWHM of the spectral density.
+        
+    cav_freq: float
+        The qubit frequency.
+        
+    beta: float
+        The inverse temperature.
+    
+    t: float
+        The time t.
+    """
+    fac = (1. - np.cos(w*t))*(coth(w*beta/2))/w**2
+    sd = -4*underdamped_brownian(w, coup_strength, cav_broad, cav_freq)
+    return sd*fac
+
+
+def pure_dephasing_evolution(tlist, coup_strength, cav_broad, cav_freq, beta, w0):
+    """
+    Compute the pure dephasing evolution using the numerical integrand.
+    """
+    integrand = lambda t: quad(pure_dephasing_integrand, 0.0, np.inf,
+                                args=(coup_strength, cav_broad, cav_freq, beta, t))
+    evolution = np.array([np.exp(1j*w0*t + integrand(t)[0]/np.pi) for t in tlist])
+    return evolution
+
+
+def pure_dephasing_evolution_analytical(tlist, w0, ck, vk):
+    """
+    Computes the propagating function appearing in the pure dephasing model.
+        
+    Parameters
+    ----------
+    t: float
+        A float specifying the time at which to calculate the integral.
+    
+    wq: float
+        The qubit frequency in the Hamiltonian.
+
+    ck: ndarray
+        The list of coefficients in the correlation function.
+        
+    vk: ndarray
+        The list of frequencies in the correlation function.
+    
+    Returns
+    -------
+    integral: float
+        The value of the integral function at time t.
+    """
+    evolution = np.array([np.exp(-1j*w0*t - correlation_integral(t, ck, vk)) for t in tlist])
+    return evolution
+
+def correlation_integral(t, ck, vk):
+    """
+    Computes the integral sum function appearing in the pure dephasing model.
+    
+    If the correlation function is a sum of exponentials then this sum
+    is given by:
+    
+    .. math::
+        
+        \int_0^{t}d\tau D(\tau) = \sum_k\frac{c_k}{\mu_k^2}e^{\mu_k t}
+        + \frac{\bar c_k}{\bar \mu_k^2}e^{\bar \mu_k t}
+        - \frac{\bar \mu_k c_k + \mu_k \bar c_k}{\mu_k \bar \mu_k} t
+        + \frac{\bar \mu_k^2 c_k + \mu_k^2 \bar c_k}{\mu_k^2 \bar \mu_k^2}
+        
+    Parameters
+    ----------
+    t: float
+        A float specifying the time at which to calculate the integral.
+    
+    ck: ndarray
+        The list of coefficients in the correlation function.
+        
+    vk: ndarray
+        The list of frequencies in the correlation function.
+    
+    Returns
+    -------
+    integral: float
+        The value of the integral function at time t.
+    """
+    t1 = np.sum(np.multiply(np.divide(ck, vk**2), np.exp(vk*t) - 1))
+    t2 = np.sum(np.multiply(np.divide(np.conjugate(ck), np.conjugate(vk)**2),
+                            np.exp(np.conjugate(vk)*t) - 1))
+    t3 = np.sum((np.divide(ck, vk) + np.divide(np.conjugate(ck),
+                                               np.conjugate(vk)))*t)
+    return 2*(t1+t2-t3)
