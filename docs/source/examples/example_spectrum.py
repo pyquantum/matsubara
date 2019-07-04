04ceef485fada88eaf2b36a16f338ddff5640ab3
Neill Lambert <nwlambert@gmail.com>
2019-07-04 16:09:34 +0900

    Revert "Revert "added constrained biexp fit""
    This reverts commit d5026397b3cba7bc3a65737bd417c55d760f4d48.

--- docs/source/examples/example_spectrum.py
+++ docs/source/examples/example_spectrum.py
@@ -7,23 +7,23 @@ from matsubara.correlation import spectrum_matsubara, spectrum_non_matsubara, sp
 import matplotlib.pyplot as plt
 
 
-cav_freq = 1.
-cav_broad = 0.5
+bath_freq = 1.
+bath_broad = 0.5
 coup_strength = 1
 
 # High temperature case
-beta = 0.001
+beta = 0.1
 w = np.linspace(-5, 10, 200)
 
-matsu_spectrum = spectrum_matsubara(w, coup_strength, cav_broad, cav_freq, beta)
-total_spectrum = spectrum(w, coup_strength, cav_broad, cav_freq, beta)
+matsu_spectrum = spectrum_matsubara(w, coup_strength, bath_broad, bath_freq, beta)
+total_spectrum = spectrum(w, coup_strength, bath_broad, bath_freq, beta)
 
-nonmatsu_spectrum = spectrum_non_matsubara(w, coup_strength, cav_broad, cav_freq, beta)
-nonmatsu_spectrum_neg = spectrum_non_matsubara(-w, coup_strength, cav_broad, cav_freq, beta)
+nonmatsu_spectrum = spectrum_non_matsubara(w, coup_strength, bath_broad, bath_freq, beta)
+nonmatsu_spectrum_neg = spectrum_non_matsubara(-w, coup_strength, bath_broad, bath_freq, beta)
 
 # Effective temperature
 log = np.log(nonmatsu_spectrum/nonmatsu_spectrum_neg)
-effective_beta = log/(w*beta)
+effective_beta = log/(w)
 
 plt.plot(w, total_spectrum, label = r"S(total)", color = "blue")
 plt.plot(w, nonmatsu_spectrum, "--", label = r"$S_0(\omega)$", color = "orange")
@@ -36,23 +36,23 @@ plt.figure(figsize=(5, 4))
 plt.plot(w[w>0], effective_beta[w>0])
 plt.ylim(-2, 2)
 plt.xlabel(r"$\omega$")
-plt.ylabel(r"$\beta_{eff}[\beta]$")
+plt.ylabel(r"$\beta_{eff}[\omega]$")
 plt.show()
 
 
 # Low temperature case
-beta = 100
+beta = np.inf
 w = np.linspace(-5, 10, 200)
 
-matsu_spectrum = spectrum_matsubara(w, coup_strength, cav_broad, cav_freq, beta)
-total_spectrum = spectrum(w, coup_strength, cav_broad, cav_freq, beta)
+matsu_spectrum = spectrum_matsubara(w, coup_strength, bath_broad, bath_freq, beta)
+total_spectrum = spectrum(w, coup_strength, bath_broad, bath_freq, beta)
 
-nonmatsu_spectrum = spectrum_non_matsubara(w, coup_strength, cav_broad, cav_freq, beta)
-nonmatsu_spectrum_neg = spectrum_non_matsubara(-w, coup_strength, cav_broad, cav_freq, beta)
+nonmatsu_spectrum = spectrum_non_matsubara(w, coup_strength, bath_broad, bath_freq, beta)
+nonmatsu_spectrum_neg = spectrum_non_matsubara(-w, coup_strength, bath_broad, bath_freq, beta)
 
 # Effective temperature
 log = np.log(nonmatsu_spectrum/nonmatsu_spectrum_neg)
-effective_beta = log/(w*beta)
+effective_beta = log/(w)
 
 plt.plot(w, total_spectrum, label = r"$S(full)$", color = "blue")
 plt.plot(w, nonmatsu_spectrum, "--", label = r"$S_0(\omega)$", color = "orange")
@@ -63,7 +63,6 @@ plt.show()
 
 plt.figure(figsize=(5, 4))
 plt.plot(w[w>0], effective_beta[w>0])
-plt.ylim(-.1, .1)
 plt.xlabel(r"$\omega$")
-plt.ylabel(r"$\beta_{eff}[\beta]$")
+plt.ylabel(r"$\beta_{eff}[\omega]$")
 plt.show()
